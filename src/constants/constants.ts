06186d3cbdaadae7634a351e3bb83be82de7a6e5
Sofiia Novychenko <milakotiy@gmail.com>
2025-08-31 19:35:49 +0200

    add donation card ui; add icons folder in src; change next.config for images and svgr

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -17,7 +17,7 @@ export const MENU_ARRAY = [
   },
   {
     title: 'Link-button',
-    link:'link-button',
+    link: 'link-button',
   },
   {
     title: 'Адміністратор',
@@ -29,3 +29,5 @@ export const COOKIES_VALUE = {
   usual: 'usual',
   super: 'super',
 };
+
+export const BASE_IMAGE_URL = 'https://inharmony-v2.h.goit.study/images/all/';
