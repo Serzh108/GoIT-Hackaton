083d433ebec1e497f807b7096396bbdf8686b744
Serzh108 <serzh108@gmail.com>
2025-09-06 22:36:05 +0300

    ADD | begining

--- src/components/Reporting/ReportEditing.tsx
+++ src/components/Reporting/ReportEditing.tsx
@@ -1,46 +1,45 @@
 'use client';
-
-import { yupResolver } from "@hookform/resolvers/yup";
 import { FC } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 type Props = {
     id?: string;
 };
 
 const ReportEditing: FC<Props> = ({id}) => {    
-  const users = useUserStore(state => state.users);
-  const user = users.find(item => item._id === id); 
+  // const users = useUserStore(state => state.users);
+  // const user = users.find(item => item._id === id); 
  
-  const initialValues: IRegisterFormData = {
-    role: user?.role || "editor",
-    name: user?.name || "",
-    email: user?.email || "",
-    password: "",
-  };
+  // const initialValues: IRegisterFormData = {
+  //   role: user?.role || "editor",
+  //   name: user?.name || "",
+  //   email: user?.email || "",
+  //   password: "",
+  // };
 
-    const {
-      register,
-      handleSubmit,
-      reset,
-      control,
-      formState: { errors },
-      } = useForm<IRegisterFormData>({
-      resolver: yupResolver(RegisterFormSchema),
-      defaultValues: initialValues,
-    });
+    // const {
+    //   register,
+    //   handleSubmit,
+    //   reset,
+    //   control,
+    //   formState: { errors },
+    //   } = useForm<IRegisterFormData>({
+    //   resolver: yupResolver(RegisterFormSchema),
+    //   defaultValues: initialValues,
+    // });
 
-   const handleEditing = async (data: IRegisterFormData) => {
-        console.log('handleEditing data -> ', data);
-        if (isFetching) {
-        return;
-        };
-    };
+  //  const handleEditing = async (data: IRegisterFormData) => {
+  //       console.log('handleEditing data -> ', data);
+  //       if (isFetching) {
+  //       return;
+  //       };
+  //   };
 
      return(
     <div className="p-12 pt-20 ml-[20px]">
       <form
-        onSubmit={handleSubmit(handleEditing)}
+        // onSubmit={handleSubmit(handleEditing)}
         className="text-xl leading-6"
       ></form>
       </div>
