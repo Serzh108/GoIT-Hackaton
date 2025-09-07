f98bca0d53f6c8aa924d08ad667489145a44fb89
Serzh108 <serzh108@gmail.com>
2025-09-07 12:23:31 +0300

    ADD | after merge with dev

--- src/app/(pages)/reporting/page.tsx
+++ src/app/(pages)/reporting/page.tsx
@@ -3,8 +3,8 @@ import PageHeader from "@/components/PageHeader/PageHeader";
 const Reporting = () => {
   return (
     <section className="ml-[256px]">
-      <h1>Reporting</h1>
-      <p>Page routing Reporting</p>
+      <PageHeader isShowEditeImage />
+      {/* <ReportsList /> */}
     </section>
   );
 };
